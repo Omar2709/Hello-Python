ee30c98251d92663fc43cea9e55d432e6c0ce35f
Omar2709 <omarrglopez@gmail.com>
2026-07-29 16:54:53 -0500

    Segundo Commit"Ejercicios de Tipos de datos"

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# Primer Hola mundo en python 
 print("Hello, Python!")
+
+# Consultar el tipo de dato 
+print(type("Hello, Python!"))
+print(type(10))
+print(type(10.5))
+print(type(1 + 2j))
+print(type([1, 2, 3]))
+print(type(True))
+
+print("Donde vivies ?")
+Ubicacion = input()
+print("Vives en: " + Ubicacion)
